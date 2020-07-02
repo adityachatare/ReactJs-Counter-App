9ba6a76e180d4709a25bbd35da1198fdf50f1d20
Aditya Chatare <adityachatare1@gmail.com>
2020-07-02 18:03:07 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+import './App.css';
+
+
+function App(){
+
+  const[count, setCount] = useState(0); 
+  return(
+    <div>
+      <div className="App">
+        <header>
+        <h1>Counter App using State</h1>
+        </header>
+        <h2>Current value of count is {count}</h2>
+        <button onClick={()=> setCount(0)}>Reset Counter</button>
+        <button onClick={()=>setCount(count+1)}>Increase Counter</button>
+        <button onClick={()=>setCount(count - 1)}>Decrese Counter</button>
+      </div>
+    </div>
+  );
+}
+export default App;
